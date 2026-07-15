0d9c7c4e26165fab22f587bed696a705aeec4576
jyotkotadiya-code <jyotkotadiya-code@users.noreply.github.com>
2026-07-15 16:18:05 +0530

    feat: integrate Vercel Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "sonner";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -41,6 +42,7 @@ export default function RootLayout({
         >
           {children}
           <Toaster richColors position="top-center" theme="system" />
+          <SpeedInsights />
         </ThemeProvider>
       </body>
     </html>
